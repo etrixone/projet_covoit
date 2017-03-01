7c78aa09fa1e775091e47a263f602758e38b0097
Christophe <Christophe@DESKTOP-74V3J1K.home>
2017-03-01 16:03:41 +0100

    ajout page all-user côté admin

--- resources/views/all-users.blade.php
+++ resources/views/all-users.blade.php
@@ -3,18 +3,24 @@
 @section('content')
 
 <html>
-    <body>
-        <table>
+    <body>        
+        <table border="1">
             <tr>
+                <th>Identifiant</th>
                 <th>Nom</th>
                 <th>Prenom</th>
                 <th>Email</th>
+                <th>Dernière connexion</th>
+                <th>Supprimer</th>
             </tr>
             @foreach($users as $user)
             <tr>
+                <td>{{$user->id}}</td>
                 <td>{{$user->name}}</td>
                 <td>{{$user->surname}}</td>
                 <td>{{$user->email}}</td>  
+                <td>{{$user->last_connection}}</td>  
+                <td><a href="{{url('admin/delete/{id}')}}">Supprimer</a></td>  
             </tr>
             @endforeach 
         </table>
