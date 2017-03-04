ee3b99265d9d15bff637d6c4653b7abe9e01be6b
Bryan <Bryan@PC>
2017-03-04 16:38:07 +0100

    app2 devient le layout du site saliege covoit !

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -11,7 +11,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="http://localhost/laravel/covoiturage/public/css/bootstrap.css" rel="stylesheet">
+    <link href="http://localhost/laravel/covoiturage/public/css/style.css" rel="stylesheet">
+    
+    <!-- Import police --> 
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+    
+    <!--<link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
      <link rel="stylesheet" href="http://localhost/laravel/covoiturage/public/css/addresspicker/jquery.ui.all.css">
      
     <!-- Scripts -->
@@ -23,76 +30,47 @@
     </script>
     
     <script src="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyC8zSQHYetf1-fRjNQCy7aYDwT4SCR2Xo0"></script>
-
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
     <script src="http://localhost/laravel/covoiturage/public/js/jquery.ui.addresspicker.js"></script>
-    
-    
-    <!--Creation du picker-->
+
 
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+  <div class="container-fluid">
+      <header class="row">
+       <div class="logo">
+           <img src="http://localhost/laravel/covoiturage/public/images/Logo2.gif">
+       </div>
+       <div class="logo2">
+          <img src="http://localhost/laravel/covoiturage/public/images/Utilisateur2.gif">
+           <p>Stacy Boulanger</p>
+       </div>
+        <!--<img src="http://localhost/laravel/covoiturage/public/images/logo3.gif" class="logo">-->
+         <div class="barre"></div>
+      </header>
+      <nav class="row">
+          <div>
+              <ul>
+                  <li class="b1" class="l1"><a href="#">Rechercher un trajet</a></li>
+                  <li class="b2"><a href="#">Mes réservations</a></li>
+                  <li class="b3"><a href="#">Proposer un trajet</a></li>
+                  <li class="b4"><a href="#">Mes trajets</a></li>
+                  <li class="b5"><a href="#">Contact</a></li>
+              </ul>
+          </div>
+      </nav>
+      <section class="row">
+          <div>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        
-    </div>
+               @yield('content')
     
-    @yield('content')
+          </div>
+      </section>
+      <footer class="row">
+      
+      </footer>
+  </div>
    
 </body>
 </html>
