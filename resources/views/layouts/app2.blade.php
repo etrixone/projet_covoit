4d889dcddbe6071e630bf16a43dff406c16fd9c5
cheeper <cheeper@192.168.1.41>
2017-03-11 15:48:52 +0100

    rechercher trajet: modif style + ajout bouton date

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,21 +15,23 @@
     <!-- Bootstrap -->
     <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
-    
-    <!-- Import police --> 
+
+    <!-- Import police -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
-    
+
     <!--<link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
     <!-- <link rel="stylesheet" href="http://localhost/laravel/covoiturage/public/css/addresspicker/jquery.ui.all.css">-->
-     
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
+        window.Laravel = {
+            !!json_encode([
+                'csrfToken' => csrf_token(),
+            ]) !!
+        };
     </script>
-    
+
     <!--<script src="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyC8zSQHYetf1-fRjNQCy7aYDwT4SCR2Xo0"></script>-->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
@@ -36,42 +39,44 @@
 
 
 </head>
-<body>
-  <div class="container-fluid">
-      <header class="row">
-       <div class="logo">
-           <img src="{{ asset('/images/Logo2.gif') }}">
-       </div>
-       <div class="logo2">
-          <img src="{{ asset('/images/Utilisateur2.gif') }}">
-           <p class="prenom">{{ Auth::user()->surname }}</p>
-           <p class="nom">{{ Auth::user()->name }}</p>
-       </div>
-        <!--<img src="http://localhost/laravel/covoiturage/public/images/logo3.gif" class="logo">-->
-         <div class="barre"></div>
-      </header>
-      <nav class="row">
-          <div>
-              <ul>
-                  <li class="b1"><a href="{!! url('rechercher_un_trajet') !!}">Rechercher un trajet</a></li>
-                  <li class="b2"><a href="#">Mes réservations</a></li>
-                  <li class="b3"><a href="{!! url('proposer_un_trajet') !!}">Proposer un trajet</a></li>
-                  <li class="b4"><a href="#">Mes trajets</a></li>
-                  <li class="b5"><a href="#">Contact</a></li>
-              </ul>
-          </div>
-      </nav>
-      <section class="row">
-          <div>
 
-               @yield('content')
-    
-          </div>
-      </section>
-      <footer class="row">
-      
-      </footer>
-  </div>
-   
+<body>
+    <div class="container-fluid">
+        <header class="row">
+            <div class="logo">
+                <img src="{{ asset('/images/Logo2.gif') }}">
+            </div>
+            <div class="logo2">
+                <img src="{{ asset('/images/Utilisateur2.gif') }}">
+                <p class="prenom">{{ Auth::user()->surname }}</p>
+                <p class="nom">{{ Auth::user()->name }}</p>
+            </div>
+            <!--<img src="http://localhost/laravel/covoiturage/public/images/logo3.gif" class="logo">-->
+            <div class="barre"></div>
+        </header>
+        <nav class="row">
+            <div>
+                <ul>
+                    <li class="b1"><a href="{!! url('rechercher_un_trajet') !!}">Rechercher un trajet</a></li>
+                    <li class="b2"><a href="#">Mes réservations</a></li>
+                    <li class="b3"><a href="{!! url('proposer_un_trajet') !!}">Proposer un trajet</a></li>
+                    <li class="b4"><a href="#">Mes trajets</a></li>
+                    <li class="b5"><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+        <section class="row">
+            <div>
+
+                @yield('content')
+
+            </div>
+        </section>
+        <footer class="row">
+
+        </footer>
+    </div>
+
 </body>
+
 </html>
--- resources/views/rechercher-un-trajet.blade.php
+++ resources/views/rechercher-un-trajet.blade.php
@@ -1,58 +1,66 @@
-@extends('layouts.app2')
+@extends('layouts.app2') @section('content')
 
-@section('content')    
-      
-    <center>
-        <h1>Bienvenue sur saliege covoit</h1> 
-                <br/><br/>
-    	<form method="POST" action="{!! url('rechercher_un_trajet') !!}" accept-charset="UTF-8">
-		{!! csrf_field() !!}   
-	
-           <label>Départ : </label> <input id="depart" type="text"/> 
-           <input id="localityDepart" name="depart" type="hidden"/><br/> 
-           
-           <label>Destination : </label> <input id="destination" type="text"/> 
-           <input id="localityDestination" name="destination" type="hidden"/><br/> 
+<center>
+    <h1>Bienvenue sur saliege covoit</h1>
+    <br/>
+    <br/>
+    <form method="POST" action="{!! url('rechercher_un_trajet') !!}" accept-charset="UTF-8">
+        {!! csrf_field() !!}
 
+        <div class="flex">
+            <label>Départ </label>
+            <hr class="flex-barre flex-barre1">
+            <input id="depart" type="text" class="form-control input-sm">
+            <input id="localityDepart" name="depart" type="hidden">
+            <br/>
+        </div>
 
-            <input type="submit" value="Rechercher">	
-            
-            @if (count($errors) > 0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
-     
-                @if(!empty($resultat))
-                    <h2>IL Y A DES TRAJETS</h2>
-                    
-                    @foreach ($resultat as $trajet)
-                        <p>Depart: {{ $trajet->TRJ_DEPART }} </p> <br>
-                        <p>Destination: {{ $trajet->TRJ_DESTINATION }} </p> <br>
-                        <p>Info: {{ $trajet->TRJ_INFO }} </p> <br>
-                    @endforeach
-                @endif
+        <div class="flex">
+            <label>Destination </label>
+            <hr class="flex-barre flex-barre2">
+            <input id="destination" type="text" class="form-control input-sm">
+            <input id="localityDestination" name="destination" type="hidden">
+            <br/>
+        </div>
 
-        </form>
+        <div class="flex">
+            <label>Date </label>
+            <hr class="flex-barre flex-barre3">
+            <input type="date" id="date" class="form-control input-sm">
+        </div>
+        <br>
+        <input type="submit" value="Rechercher" class="rechercher"> @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif @if(!empty($resultat))
+        <h2>IL Y A DES TRAJETS</h2> @foreach ($resultat as $trajet)
+        <p>Depart: {{ $trajet->TRJ_DEPART }} </p>
+        <br>
+        <p>Destination: {{ $trajet->TRJ_DESTINATION }} </p>
+        <br>
+        <p>Info: {{ $trajet->TRJ_INFO }} </p>
+        <br> @endforeach @endif
 
-    </center>
+    </form>
+
+</center>
 
 <script>
-    
-    function initMap(){
+    function initMap() {
         var origin_input = document.getElementById('depart');
         var destination_input = document.getElementById('destination');
 
         var origin_autocomplete = new google.maps.places.Autocomplete(origin_input);
         var destination_autocomplete =
-        new google.maps.places.Autocomplete(destination_input);
-    
+            new google.maps.places.Autocomplete(destination_input);
+
         function fillInAddressDepart() {
-    // Get the place details from the autocomplete object.
+            // Get the place details from the autocomplete object.
             var place = origin_autocomplete.getPlace();
             //init input Form
             document.getElementById('localityDepart').value = '';
@@ -61,9 +69,10 @@
             var val = place.address_components[0]['long_name'];
             document.getElementById('localityDepart').value = val;
         }
+
         function fillInAddressDestination() {
-        // Get the place details from the autocomplete object.
-        var place = destination_autocomplete.getPlace();
+            // Get the place details from the autocomplete object.
+            var place = destination_autocomplete.getPlace();
             //init input Form
             document.getElementById('localityDestination').value = '';
             document.getElementById('localityDestination').disabled = false;
@@ -71,14 +80,13 @@
             var val = place.address_components[0]['long_name'];
             document.getElementById('localityDestination').value = val;
         }
-        
+
         origin_autocomplete.addListener('place_changed', fillInAddressDepart);
         destination_autocomplete.addListener('place_changed', fillInAddressDestination);
-        
+
     }
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC8zSQHYetf1-fRjNQCy7aYDwT4SCR2Xo0&signed_in=true&libraries=places&callback=initMap"
-        async defer></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC8zSQHYetf1-fRjNQCy7aYDwT4SCR2Xo0&signed_in=true&libraries=places&callback=initMap" async defer></script>
 
-    
+
 @endsection
