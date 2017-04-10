9c37b2e3146318a67eb9bf10c1684b8c26f49525
Bryan <Bryan@PC>
2017-04-10 22:24:07 +0200

    ajoute des nouveaux logo

--- resources/views/modifier-profil.blade.php
+++ resources/views/modifier-profil.blade.php
@@ -76,11 +76,11 @@
         </div>
     </form>
 
-    
+
     <form action="{!! url('modifier_logo') !!}" method="post" accept-charset="UTF-8">
         {!! csrf_field() !!}
-        
-        
+
+
         <div class="form-profil">
 
 
@@ -160,12 +160,265 @@
                     <input type="radio" name="logo" value="/images/logo/Cheval4.png">
                 </div>
             </div>
-        </div>
-            <div class="col-sm-6">
-                <input type="submit" class="btn modifier" value="Confirmer">
+
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Chien1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Chien1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Chien2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Chien2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Chien3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Chien3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Chien4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Chien4.png">
+                </div>
             </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Ecureuil1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Ecureuil1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Ecureuil2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Ecureuil2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Ecureuil3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Ecureuil3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Ecureuil4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Ecureuil4.png">
+                </div>
+            </div>     
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Gorille1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Gorille1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Gorille2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Gorille2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Gorille3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Gorille3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Gorille4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Gorille4.png">
+                </div>
+            </div>  
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Guepard1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Guepard1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Guepard2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Guepard2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Guepard3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Guepard3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Guepard4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Guepard4.png">
+                </div>
+            </div>  
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Hippo1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Hippo1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Hippo2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Hippo2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Hippo3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Hippo3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Hippo4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Hippo4.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Lezard1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Lezard1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Lezard2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Lezard2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Lezard3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Lezard3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Lezard4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Lezard4.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Lion1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Lion1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Lion2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Lion2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Lion3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Lion3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Lion4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Lion4.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Loup1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Loup1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Loup2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Loup2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Loup3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Loup3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Loup4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Loup4.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Panda1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Panda1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Panda2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Panda2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Panda3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Panda3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Panda4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Panda4.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Papillon1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Papillon1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Papillon2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Papillon2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Papillon3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Papillon3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Papillon4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Papillon4.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Poisson1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Poisson1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Poisson2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Poisson2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Poisson3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Poisson3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Poisson4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Poisson4.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Rhino1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Rhino1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Rhino2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Rhino2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Rhino3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Rhino3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Rhino4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Rhino4.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Souris1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Souris1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Souris2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Souris2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Souris3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Souris3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Souris4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Souris4.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Taureau1.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Taureau1.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Taureau2.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Taureau2.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Taureau3.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Taureau3.png">
+                </div>
+                <div class="col-sm-3">
+                    <img src="{{ asset('/images/logo/Taureau4.png') }}" width="50px">
+                    <input type="radio" name="logo" value="/images/logo/Taureau4.png">
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <input type="submit" class="btn modifier" value="Confirmer">
+        </div>
     </form>
 
-            </section>
+</section>
 
-            @endsection
+@endsection
