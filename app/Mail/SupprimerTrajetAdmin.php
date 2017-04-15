fe5f7169d96f74426c3e12644872952eeb6ba7e2
Christophe <Christophe@DESKTOP-74V3J1K.home>
2017-04-15 10:06:49 +0200

    mail php artisan

--- app/Mail/SupprimerTrajetAdmin.php
+++ app/Mail/SupprimerTrajetAdmin.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class AjoutTrajet extends Mailable
+class SupprimerTrajetAdmin extends Mailable
 {
     use Queueable, SerializesModels;
 
